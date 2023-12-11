5c60f117bb6bb523cf431f66cdf7c46cca1a2160
audunsolemdal <audun@solom.no>
2023-12-11 12:56:32 +0100

    Support role assignments for user assigned identities

--- modules/container-web-app/r-appinsights.tf
+++ modules/container-web-app/r-appinsights.tf
@@ -26,6 +26,6 @@ resource "azurerm_application_insights" "app_insights" {
 resource "azurerm_role_assignment" "appinsights_publisher" {
   count                = var.application_insights_enabled && var.skip_identity_role_assignments == false ? 1 : 0
   scope                = local.app_insights.id
-  principal_id         = azurerm_linux_web_app.app_service_linux_container.identity[0].principal_id
+  principal_id         = try(var.identity.identity_ids[0], azurerm_linux_web_app.app_service_linux_container.identity[0].principal_id)
   role_definition_name = "Monitoring Metrics Publisher"
 }
