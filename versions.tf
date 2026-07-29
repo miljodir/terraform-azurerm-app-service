01c2a1ad3926383f9f606093e88bf7f15275e029
Audun Solemdal <audun@solom.no>
2026-07-29 08:18:30 +0200

    Make module work with azurerm 5.0 provider

--- versions.tf
+++ versions.tf
@@ -4,7 +4,7 @@ terraform {
     # tflint-ignore: terraform_unused_required_providers
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "> 3.0, < 5.0"
+      version = "> 4.0, < 6.0"
     }
     # tflint-ignore: terraform_unused_required_providers
     azurecaf = {
